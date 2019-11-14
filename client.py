66b4561de65488e1154d5147970026d214edee7d
HSabbar <36317041+HSabbar@users.noreply.github.com>
2019-11-14 15:21:06 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,50 @@
+import cv2
+import io
+import socket
+import struct
+import time
+import pickle
+import zlib
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 848))
+connection = client_socket.makefile('wb')
+
+cam = cv2.VideoCapture(0)
+
+cam.set(3, 320);
+cam.set(4, 240);
+
+img_counter = 0
+
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+while True:
+
+    ret, frame = cam.read()
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+#    data = zlib.compress(pickle.dumps(frame, 0))
+    data = pickle.dumps(frame, 0)
+    size = len(data)
+
+    
+
+    #print("{}: {}".format(img_counter, size))
+    client_socket.sendall(struct.pack(">L", size) + data)
+    img_counter += 1
+    #client side
+    data1 = client_socket.recv(1024)
+    # decode to unicode string 
+    strings = str(data1, 'utf8')
+    #get the num
+    num = int(strings)
+    if num != 0: 
+        print(num)
+    if num == 404:
+        break
+
+
+
+    
+
+cam.release()
